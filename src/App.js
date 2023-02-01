160ada355d77a77f89516acec79e49c73c8d9b22
Mateusz Kaczorowski <mmssfd0@gmail.com>
2023-02-01 13:18:31 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import Hero from './components/Hero/Hero.js';
+import SearchForm from './components/SearchForm/SearchForm.js';
+import List from './components/List/List.js';
+import Container from './components/Conteiner/Container.js';
+
+
+const App = () => {
+  return (
+    <div>
+      <Container>
+      <Hero />
+      <SearchForm />
+      <List />
+      </Container>
+    </div>
+  );
+};
+
+export default App;
---
+++ src/components/SearchForm/SearchForm.js
@@ -0,0 +1,18 @@
+import styles from './SearchForm.module.scss'
+import TextInput from '../TextInput/TextInput';
+import Button from '../Button/Button';
+
+const SearchForm = () => {
+    return (
+        <form className={styles.searchForm}>
+            <TextInput placeholder="Search..." />
+            <TextInput placeholder="Add new column" />
+            <Button>
+                <span className="fa fa-search" />
+            </Button>
+            {/* <button className={styles.button}>Search</button> */}
+        </form>
+    );
+  };
+
+export default SearchForm;
